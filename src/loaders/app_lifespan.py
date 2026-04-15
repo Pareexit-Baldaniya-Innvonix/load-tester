9fff1b6ae5b5137989ef732ef330ec75990b10c3
Pareexit-Baldaniya-Innvonix <pareexit.baldaniya@innvonix.in>
2026-04-15 14:04:42 +0530

    update the script

--- src/loaders/app_lifespan.py
+++ src/loaders/app_lifespan.py
@@ -4,7 +4,6 @@ from tortoise.contrib.fastapi import RegisterTortoise
 
 # Load logger
 from .database import (
-    generate_db_schemas,
     TORTOISE_CONFIG,
     close_db_conn,
 )
@@ -13,32 +12,22 @@ from .logging import get_logger
 logger = get_logger("app_start_up")
 
 
-async def app_shutdown_event_handler():
-    logger.debug("Closing database connection")
-    await close_db_conn()
-    logger.info("Shutting down API server")
-
-
 @asynccontextmanager
 async def app_lifespan(app: FastAPI):
-    logger.debug("Initialzing db connection")
+    logger.debug("Initializing db connection")
     try:
         async with RegisterTortoise(
             app=app,
             config=TORTOISE_CONFIG,
-            modules={"src": ["src.models"]},
             generate_schemas=True,
             add_exception_handlers=True,
         ):
-            await generate_db_schemas()
             logger.info("Connected to database")
-
             logger.info("Listening for API events")
-            # The main yield
             yield
     except Exception as e:
         logger.error(f"Error starting up API server: {str(e)}")
-        raise e
-
-    # Clean up
-    await app_shutdown_event_handler()
+        raise
+    finally:
+        await close_db_conn()
+        logger.info("Shutting down API server")
